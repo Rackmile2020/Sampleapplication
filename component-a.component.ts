adadf8b4cd950fee11488809192d2bcad56c94c8
RackMile <161143023+Rackmile2020@users.noreply.github.com>
2025-07-30 10:38:19 -0400

    Update component-a.component.ts

--- component-a.component.ts
+++ component-a.component.ts
@@ -1,38 +1,60 @@
-
-import { Component } from '@angular/core';
-import { SearchService } from '../../core/search.service';
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-component-a',
   templateUrl: './component-a.component.html',
   styleUrls: ['./component-a.component.css']
 })
-export class ComponentAComponent {
-  searchText = '';
-  currentIndex = 0;
-
-  dataA = [
-    { head: 'Alpha A', body: 'Body text for first A entry.' },
-    { head: 'Beta A',  body: 'Another body content from A.' }
+export class ComponentA implements OnInit {
+  searchTerm = '';
+  expandedItems: boolean[] = [];
+  accordionData = [
+    { title: 'First Item', content: 'This is the content for the first item' },
+    { title: 'Second Item', content: 'This is the second content' },
+    { title: 'Third Item', content: 'Another piece of content here' }
   ];
 
-  constructor(public search: SearchService) {}
+  constructor(private searchService: SearchService) {
+    this.expandedItems = new Array(this.accordionData.length).fill(false);
+  }
+
+  ngOnInit() {
+    this.searchService.currentSearchTerm.subscribe(term => {
+      if (!term) {
+        this.expandedItems = new Array(this.accordionData.length).fill(false);
+      }
+    });
+
+    this.searchService.currentActiveMatchIndex.subscribe(index => {
+      const activeMatch = this.searchService.getActiveMatchPosition();
+      if (activeMatch && activeMatch.component === 'componentA') {
+        this.expandedItems = new Array(this.accordionData.length).fill(false);
+        this.expandedItems[activeMatch.itemIndex] = true;
+      }
+    });
+  }
 
   onSearch() {
-    this.search.setTerm(this.searchText.trim());
+    this.searchService.updateSearchTerm(this.searchTerm);
   }
 
-  clear() {
-    this.searchText = '';
-    this.search.clear();
+  clearSearch() {
+    this.searchTerm = '';
+    this.searchService.clearSearch();
   }
 
-  navigate(dir: 'up' | 'down') {
-    this.search.navigate(dir);
-    this.currentIndex = this.search.currentIndex;
+  navigateUp() {
+    this.searchService.navigateMatch('up');
   }
 
-  getCount() {
-    return this.search.getCount();
+  navigateDown() {
+    this.searchService.navigateMatch('down');
+  }
+
+  toggleItem(index: number) {
+    if (!this.searchTerm) {
+      this.expandedItems[index] = !this.expandedItems[index];
+    }
   }
 }
